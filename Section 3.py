8aed59f8a26b1eda1d3240ea67eb3763c7171013
JamesU117 <43483075+JamesU117@users.noreply.github.com>
2019-10-04 15:54:02 -0700

    Create Section 3.py
    This includes the notes for section three parts 13-22 covering numbers, functions and variables

---
+++ Section 3.py
@@ -0,0 +1,55 @@
+# # #Fundamental data types
+# # #int
+# # #float
+# # #bool
+# # #str
+# # #list
+# # #tuple
+# # #set
+# # #dict
+
+# # #Classes -> custom types
+
+# # #Specialized Data Types
+
+# # #None
+
+# # #
+# # #
+# # #
+# # #
+# # #PART 16 NUMBERS
+
+# # # print(2 + 4)
+# # # print(2 - 4)
+# # # print(2 / 4)
+# # # print(2 * 4)
+
+# # # print(type(2 + 4))
+# # # print(type(2 - 4))
+# # # print(type(2 / 4))
+# # # print(type(2 * 4))
+
+# # # print(2 ** 3) #to the power of
+# # # print(2 // 3) #returns int rounded down
+# # # print (5 % 3) #mod
+
+# # #MATH  FUNCTIONS
+# # print(round(3.9)) #rounds number up
+# # print(abs(-20))   #gives absolute value
+
+# # bin(5) #will return the binary value of the number
+# # print(int('b101', 2)) #this will convert the number into int from binary
+
+# # #VARIABLES
+
+# iq = 190
+# user_age = iq/4
+# a = iq/user_age
+
+
+# print(a)
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
