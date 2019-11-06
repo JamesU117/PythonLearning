a83f39aa7a532398e3583769dfd037f29adb03ae
JamesU117 <43483075+JamesU117@users.noreply.github.com>
2019-11-05 23:52:05 -0800

    Update and rename Section 3 to Section 4.py
    Changed name to the right section and added parts 60-63

---
+++ Section 4.py
@@ -0,0 +1,37 @@
+### CONDITIONAL LOGIC
+
+# Is_Old = True
+# Is_Licensed = True
+
+# if Is_Old:
+#     print('You are old enough to drive!')
+# elif Is_Licensed:
+#     print('You can drive now!')
+# else:
+#     print('You Can not drive!')
+
+# print('Check ')
+
+# #or
+# if Is_Old and Is_Licensed:
+#     print('You can drive!')
+# else:
+#     print('You can not drive!')
+#Ternary operators
+# Condition_If_True if Condition else Condition_Is_False
+
+# Is_Friend = False
+# Can_Message = "Message allowed" if Is_Friend else "Not allowed to message"
+
+# print (Can_Message)
+
+# is_magician = True
+# is_expert = False
+# if not is_magician:
+#   print('You need magic powers')
+
+# elif not is_expert:
+#   print('At least you are getting there')
+
+# elif is_expert and is_magician:
+#   print('You are a master magician')
